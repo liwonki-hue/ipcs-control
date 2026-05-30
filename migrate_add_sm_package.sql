b1ec61e27ccc391907e3c6c306aac6e540a708fb
WONKEE <liwonki@gmail.com>
2026-05-30 06:33:43 +0500

    chore: remove all sql files from tracking

--- migrate_add_sm_package.sql
+++
@@ -1,3 +0,0 @@
--- support_master 테이블에 package 컬럼 추가
-ALTER TABLE construction.support_master
-  ADD COLUMN IF NOT EXISTS package TEXT;
--- migrate_add_vt.sql
+++
@@ -1,5 +0,0 @@
--- Add VT inspection columns to joint_master
--- Run in Supabase SQL Editor (liwonki project)
-ALTER TABLE construction.joint_master
-  ADD COLUMN IF NOT EXISTS vt_date   DATE,
-  ADD COLUMN IF NOT EXISTS vt_result TEXT;  -- 'PASS' or 'FAIL'
